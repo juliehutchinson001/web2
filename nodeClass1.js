20be3155567a4b653b2b9c49a27b9af224839d80
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-08 22:01:50 -0800

    test: learnyounode challenge 2

--- nodeClass1.js
+++ nodeClass1.js
@@ -1,4 +1,19 @@
+/* Challenge 1
 function e() {
     console.log("HELLO WORLD");
 }
 e();
+*/
+
+// Challenge 2
+//process.argv = ['/path/to/node', 'learnyournode', 'verify', 'nodeClass1.js', '1', '2', '3']
+
+function b(iterable) {
+
+    let result = 0;
+    for (let i = 2; i < iterable.length; i++) {
+        result += parseInt(iterable[i]);
+    }
+    return result;
+}
+console.log(b(process.argv));
