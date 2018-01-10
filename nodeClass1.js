5949a16df74960d7c20394324f466eb501d7b8d1
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-09 18:38:07 -0800

    feat: solution challenge 4

--- nodeClass1.js
+++ nodeClass1.js
@@ -1,16 +1,13 @@
 /* Challenge 1
 function e() {
-<<<<<<< HEAD
     //print a line that has the string "HELLO WORLD"
-=======
->>>>>>> 20be3155567a4b653b2b9c49a27b9af224839d80
     console.log("HELLO WORLD");
 }
 e();
-*/
 
-<<<<<<< HEAD
-/* Challenge 2
+------------------------------------------------------------------------
+
+Challenge 2
 process.argv = ['/path/to/node', 'learnyournode', 'verify', 'nodeClass1.js', '1', '2', '3']
 
 //the function takes an argument in the form of an array
@@ -20,24 +17,15 @@ function b(iterable) {
     //The for loop receives the array and iterate through each of the values
     for (let i = 2; i < iterable.length; i++) {
         //result picks up each item and adds it to result
-=======
-// Challenge 2
-//process.argv = ['/path/to/node', 'learnyournode', 'verify', 'nodeClass1.js', '1', '2', '3']
-
-function b(iterable) {
-
-    let result = 0;
-    for (let i = 2; i < iterable.length; i++) {
->>>>>>> 20be3155567a4b653b2b9c49a27b9af224839d80
         result += parseInt(iterable[i]);
     }
     return result;
 }
-<<<<<<< HEAD
 console.log(b(process.argv));
-*/
 
-// Challenge 3
+------------------------------------------------------------------------------
+
+Challenge 3
 //import the file module
 let fs = require('fs');
 //read the content of the file, encode it --to avoid using method toString--
@@ -46,6 +34,22 @@ let fContent = fs.readFileSync(process.argv[2], 'utf-8');
 let fContentArray = fContent.split('\n');
 //print out the length of the array - 1 to count the # of new lines
 console.log(fContentArray.length - 1);
-=======
-console.log(b(process.argv));
->>>>>>> 20be3155567a4b653b2b9c49a27b9af224839d80
+
+----------------------------------------------------------------------------------
+
+Challenge 4
+
+//import the file module
+let fs = require('fs');
+//read the content of the file, encode it and collect the value of a callback function
+let fContent = fs.readFile(process.argv[2], 'utf-8', function(err, data) {
+    //identify the data, split them as an array with \n as the 'delimiter'
+    let fContentArray = data.split('\n')
+        //print out the length of the array - 1 to count the # of new lines
+    console.log(fContentArray.length - 1);
+});
+
+----------------------------------------------------------------------------------
+*/
+
+//Challenge 5
