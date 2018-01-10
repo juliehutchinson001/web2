fc851fe22b07a2278bdd1c593ba80bdff2d2d639
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-09 17:46:35 -0800

    fix: include comments

--- nodeClass1.js
+++ nodeClass1.js
@@ -1,5 +1,6 @@
 /* Challenge 1
 function e() {
+    //print a line that has the string "HELLO WORLD"
     console.log("HELLO WORLD");
 }
 e();
@@ -8,10 +9,13 @@ e();
 /* Challenge 2
 process.argv = ['/path/to/node', 'learnyournode', 'verify', 'nodeClass1.js', '1', '2', '3']
 
+//the function takes an argument in the form of an array
 function b(iterable) {
 
     let result = 0;
+    //The for loop receives the array and iterate through each of the values
     for (let i = 2; i < iterable.length; i++) {
+        //result picks up each item and adds it to result
         result += parseInt(iterable[i]);
     }
     return result;
@@ -28,11 +32,3 @@ let fContent = fs.readFileSync(process.argv[2], 'utf-8');
 let fContentArray = fContent.split('\n');
 //print out the length of the array - 1 to count the # of new lines
 console.log(fContentArray.length - 1);
-
-
-
-/*`hello\n 
-julie\n
-hutchinson`
-
-['hello', 'julie', 'hutchinson'] - 1 */
