0d5f9bb289c79295513c8d4387ebfded8f9e9532
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-19 20:29:27 -0800

    Feat: Import post route from controller

--- reddit_clone/server.js
+++ reddit_clone/server.js
@@ -10,9 +10,11 @@ app.use(express.static("public"));
 
 app.use(bodyPser.urlencoded({ extended: true }));
 
-require('./controllers/home')(app)
+require('./controllers/home')(app);
 
-require('./controllers/post_new')(app)
+require('./controllers/post_new')(app);
+
+require('./controllers/posts')(app);
 
 app.listen("3000", () => {
     console.log("listening on port 3000");
