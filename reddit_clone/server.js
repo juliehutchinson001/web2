79518c9238ee4a3f9e118388e15cab82118d49f8
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-20 23:57:12 -0800

    fix: import home to the server

--- reddit_clone/server.js
+++ reddit_clone/server.js
@@ -2,7 +2,15 @@ let express = require("express");
 
 let bodyPser = require("body-parser");
 
+//Import the mongoose module
+let mongoose = require('mongoose');
+
+
 let app = express();
+
+mongoose.connect('mongodb://localhost/27017');
+mongoose.set('debug', true);
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
 
 app.set("view engine", "ejs");
 
