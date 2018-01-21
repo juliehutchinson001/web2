2c7e38b93e7c8dbf98d6fe541fc0a73d21f915b1
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-20 23:36:18 -0800

    refactor: post request included

--- reddit_clone/controllers/posts.js
+++ reddit_clone/controllers/posts.js
@@ -1,7 +1,13 @@
+let Post = require('../models/post')
+
 module.exports = app => {
     app.post('/posts', (req, res) => {
         // add post to database
+        let post = new Post(req.body);
 
-        res.redirect('/posts')
+        post.save((err, post) => {
+            // REDIRECT TO THE ROOT
+            res.redirect('/posts');
+        })
     })
 }
