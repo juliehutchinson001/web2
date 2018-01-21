e3113e7fecd2badb61e3654f2977c3a64c37527e
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-20 23:34:59 -0800

    refactor: combine index.js into home.js

--- reddit_clone/controllers/index.js
+++
@@ -1,9 +0,0 @@
-let Post = require('../models/index')
-
-module.exports = app => {
-    app.find({}).then((posts) => {
-        res.render('pageTitle', { posts })
-    }).catch((err) => {
-        console.log(err.message);
-    })
-}
