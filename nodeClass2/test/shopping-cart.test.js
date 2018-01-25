2b1f895aa8b2df853d7edefcfdbbdb8b7b1190ca
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-24 17:45:43 -0800

    refactor: modify the importing of modules

--- nodeClass2/test/shopping-cart.test.js
+++ nodeClass2/test/shopping-cart.test.js
@@ -1,4 +1,8 @@
 const mocha = require('mocha')
+    // const chai = require('chai').
 const expect = require('chai').expect
 
-const Item = require('../cart').Item
+//const Item = require('../cart').Item
+const { Item, getCart, addItem } = require('../cart')
+
+let testPost;
