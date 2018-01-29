e2be823bb0b621347f6e42a424d6d907f93ed627
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-29 02:39:29 -0800

    refactor: separation of concerns to run the instance of express

--- reddit_clone/controllers/post_new.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-    app.get("/posts/new", (req, res) => {
-        res.render("new_form", { pageTitle: 'Home Page' });
-    })
-}
--- reddit_clone/server/index.js
+++ reddit_clone/server/index.js
@@ -1,5 +1,10 @@
-let app = require('./server');
+let express = require('express');
 
-app.listen("3000", () => {
+let app = express();
+
+//running the instance of express
+app.listen("3000", function() {
     console.log("listening on port 3000");
 });
+
+module.exports = { app };
