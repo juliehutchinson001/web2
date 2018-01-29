cd4c1910a3d0614e984e127982d8c0754787c547
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-29 02:43:16 -0800

    refactor: distribution of code to index and routes

--- reddit_clone/server/server.js
+++ reddit_clone/server/server.js
@@ -2,15 +2,18 @@ let express = require("express");
 
 let bodyPser = require("body-parser");
 
+let routes = require("./routes");
+
 //Import the mongoose module
 let mongoose = require('mongoose');
 
 
 let app = express();
 
+app.use('/posts', routes);
+
 mongoose.connect('mongodb://localhost/27017');
 mongoose.set('debug', true);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
 
-
-module.exports = { app }
+module.exports = { app };
