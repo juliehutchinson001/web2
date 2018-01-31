4fdb2567a36fd0430eb156128c0e011b5ec96ceb
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-30 18:19:54 -0800

    chore: implement signup functionality in my schema

--- reddit_clone/server/models/post.js
+++ reddit_clone/server/models/post.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const PostSchema = new Schema({
-//     title: { type: String, required: true },
-//     url: { type: String, required: true },
-//     summary: { type: String, required: true }
-// })
-
 const PostSchema = new Schema({
     createdAt: { type: Date },
     updatedAt: { type: Date },
-    // subreddit: { type: String, required: true },
     title: { type: String, required: true },
     url: { type: String, required: true },
-    summary: { type: String, required: true }
-    // _creator: { type: Schema.ObjectId, ref: 'User', required: true },
+    summary: { type: String, required: true },
+    subreddit: { type: String, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 })
 
 PostSchema.pre('save', function(next) {
