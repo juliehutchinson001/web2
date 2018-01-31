35c300d69fe931a23e2015c033437faaa86bd733
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-01-30 18:31:06 -0800

    feat: implementation of the comments schema in reddit clone

--- reddit_clone/server/models/comments.js
+++ reddit_clone/server/models/comments.js
@@ -1,2 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+    content: { type: String, required: true }
+});
+
+CommentSchema.pre('save', function(next) {
+    const now = new Date();
+    this.updatedAt = now;
+
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
