9e8b4eb5d63c9540a82ea9f808eabbda077f236f
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-01 23:16:22 -0800

    fix: change ejs notation to correctly redirect new users

--- reddit_clone/controllers/show.js
+++ reddit_clone/controllers/show.js
@@ -1,11 +1,11 @@
 let Post = require("../models/post");
 
-module.exports = app => {
+module.exports = function(app) {
     app.get('/posts/:id', (req, res) => {
         let postId = req.params.id
         Post.findById(postId).then(post => {
             res.render('show', { post: post, pageTitle: '<%= post.title%>' })
-        }).catch((err) => {
+        }).catch(function(err) {
             console.log(err);
         })
     })
