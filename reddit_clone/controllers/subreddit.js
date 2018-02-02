0cb015e9d31a67a3c1c062ca9b7a7c4cb588ebd8
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-02 11:49:42 -0800

    feat: subreddit implementation to reddit clone

--- reddit_clone/controllers/subreddit.js
+++ reddit_clone/controllers/subreddit.js
@@ -1,1 +1,12 @@
 let Post = require('../models/post');
+
+module.exports = function(app) {
+    app.get('/n/:subreddit', function(req, res) {
+        const thisUser = req.user;
+        Post.find({ subreddit: req.params.subreddit }).then(function(posts) {
+            res.render('index', { posts: posts, pageTitle: req.params.subreddit, thisUser: thisUser })
+        }).catch(function(err) {
+            console.log(err)
+        })
+    })
+}
