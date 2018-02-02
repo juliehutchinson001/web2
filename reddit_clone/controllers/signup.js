f15a6c1a92d70a26069493a9dac4555bbcd18e25
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-02 11:52:44 -0800

    feat: user signup implementation to reddit clone

--- reddit_clone/controllers/signup.js
+++ reddit_clone/controllers/signup.js
@@ -1,2 +1,19 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+
+module.exports = function(app) {
+    // SIGN UP POST
+    app.post('/signup', function(req, res) {
+        // Create User
+        const user = new User(req.body);
+
+        user.save().then(function(user) {
+            var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "90 days" });
+            res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+            res.redirect('/posts');
+        }).catch(function(err) {
+            console.log(err.message);
+            res.redirect('/posts');
+        });
+    });
+}
