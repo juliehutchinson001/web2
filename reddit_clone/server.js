026a3a91a6702d4ff6f90d884371cfe30eb198b8
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-01 18:46:23 -0800

    fix: rerouting server

--- reddit_clone/server.js
+++ reddit_clone/server.js
@@ -2,15 +2,10 @@ let express = require("express");
 
 let bodyPser = require("body-parser");
 
-let routes = require("./routes");
-
 //Import the mongoose module
 let mongoose = require('mongoose');
 
-
 let app = express();
-
-// app.use('/posts', routes);
 
 mongoose.connect('mongodb://localhost/27017', function() {
     console.log("connected to mongodb...");
@@ -18,5 +13,25 @@ mongoose.connect('mongodb://localhost/27017', function() {
 
 mongoose.set('debug', true);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyPser.urlencoded({ extended: true }));
+
+require('./controllers/home')(app);
+
+require('./controllers/post_new')(app);
+
+require('./controllers/posts')(app);
+
+require('./controllers/show')(app);
+
+//running the instance of express
+app.listen("3000", function() {
+    console.log("listening on port 3000");
+});
+
 
 module.exports = { app };
