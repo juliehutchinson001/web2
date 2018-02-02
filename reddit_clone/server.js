ae5f56af1217d12107ce687e589e8806d7a57158
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-02 11:44:45 -0800

    chore: addition of reddit clone complete routing and importing of dependencies

--- reddit_clone/server.js
+++ reddit_clone/server.js
@@ -1,11 +1,15 @@
-let express = require("express");
+const cookieParser = require('cookie-parser');
+const bodyPser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const express = require('express');
 
-let bodyPser = require("body-parser");
+require('dotenv').config();
 
 //Import the mongoose module
 let mongoose = require('mongoose');
 
 let app = express();
+app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost/27017', function() {
     console.log("connected to mongodb...");
@@ -13,6 +17,22 @@ mongoose.connect('mongodb://localhost/27017', function() {
 
 mongoose.set('debug', true);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
+
+
+const authentic = (req, res, next) => {
+    console.log("authentication in progress");
+    if (typeof req.cookies === 'undefined' || req.cookies.nToken === 'undefined' || req.cookies.nToken === null) {
+        req.user = null;
+    } else {
+        var token = req.cookies.nToken;
+        var decodedToken = jwt.decode(token, { complete: true }) || {};
+        req.user = decodedToken.payload;
+    }
+
+    next()
+}
+app.use(authentic);
+
 
 app.set("view engine", "ejs");
 
@@ -27,6 +47,18 @@ require('./controllers/post_new')(app);
 require('./controllers/posts')(app);
 
 require('./controllers/show')(app);
+
+require('./controllers/subreddit')(app)
+
+require('./controllers/comments')(app)
+
+require('./controllers/registration')(app)
+
+require('./controllers/signup')(app)
+
+require('./controllers/logout')(app)
+
+require('./controllers/login')(app)
 
 //running the instance of express
 app.listen("3000", function() {
