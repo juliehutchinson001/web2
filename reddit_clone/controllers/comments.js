8daf7956674facf140f670a76c250de9cbee1114
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-02 00:26:33 -0800

    fix: complete functionality of comments

--- reddit_clone/controllers/comments.js
+++ reddit_clone/controllers/comments.js
@@ -1,25 +1,29 @@
-// CREATE Comment
+let Comment = require('../models/comments');
+let Post = require('../models/post');
+let User = require('../models/user');
+
+
 module.exports = function(app) {
     app.post('/posts/:postId/comments', function(req, res) {
         // INSTANTIATE INSTANCE OF MODEL
         const comment = new Comment(req.body)
+        comment.author = req.user._id
 
         // SAVE INSTANCE OF Comment MODEL TO DB
-        comment.save()
-            .then(function() {
-                return Post.findById(req.params.postId);
-            }).then((post) => {
-                if (!post.comments) {
-                    post.comments = [];
-                }
-
-                post.comments.unshift(comment);
-                post.save()
-                    .then(function(post) {
-                        res.redirect("/posts");
-                    });
-            }).catch(function(err) {
-                console.log(err.message)
-            });
-    });
-};
+        comment.save().then(function(comment) {
+            return User.findById(req.user._id);
+        }).then(function(user) {
+            user.comments.unshift(comment);
+            user.save()
+            return Post.findById(req.params.postId)
+        }).then(function(post) {
+            user.comments.unshift(comment);
+            user.save()
+        }).then(function(post) {
+            res.redirect('/posts/${req.params.postId}')
+        }).catch(function(err) {
+            console.log(err);
+            res.redirect('/posts')
+        })
+    })
+}
