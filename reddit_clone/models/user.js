d3b56c2e848af4540b522d34bc725d15abeb10af
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-02 12:11:56 -0800

    feat: implementation of encrypt to user schema

--- reddit_clone/models/user.js
+++ reddit_clone/models/user.js
@@ -1,57 +1,35 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     createdAt: { type: Date },
     updatedAt: { type: Date },
-    password: { type: String, select: false },
-    username: { type: String, required: true }
+    password: { type: String, select: false, required: true },
+    username: { type: String, required: true },
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 });
-
-
-UserSchema.methods.encryptPassword = function(plainTextPassword) {
-    if (!plainTextPassword || typeof plainTextPassword != "string") {
-        console.log("Password is not valid");
-        return ''
-    } else {
-        var salt = bcrypt.genSaltSync(10);
-        return bcrypt.hashSync(plainTextPassword, salt);
-    }
-};
-
-UserSchema.methods.comparePassword = function(attemptedPassword) {
-    if (!attemptedPassword || typeof plainTextPassword != "string") {
-        console.log("Password not acceptable (compare)");
-        return false;
-    }
-    return bcrypt.compareSync(attemptedPassword, hash);
-};
 
 UserSchema.pre('save', function(next) {
     const now = new Date();
     this.updatedAt = now;
-
     if (!this.createdAt) {
         this.createdAt = now;
     }
-
-    if (this.isModified('password')) {
-        console.log("Password updated in save method")
-        this.password = this.encryptPassword(this.password);
-    }
-    next();
-});
-
-UserSchema.pre('update', function(next) {
-    const password = this.getUpdate().$set.password;
-    if (!password) {
-        return next();
-    }
-    console.log("Password updated in update method")
-    this.getUpdate().$set.password = this.encryptPassword(password);
-    next()
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            next();
+        });
+    });
 
 });
+
+UserSchema.methods.comparePassword = function(password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema);
