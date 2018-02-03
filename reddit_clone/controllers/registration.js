016c8707d270197500f09ee52d5470b4f9868c28
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-03 01:00:49 -0800

    fix: correction to the home route for rendering purposes

--- reddit_clone/controllers/registration.js
+++ reddit_clone/controllers/registration.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     app.get('/signup', function(req, res) {
         const thisUser = req.user;
-        res.render('signup', { pageTitle: 'Signup', thisUser: thisUser })
+        res.render('signup', { pageTitle: 'Reddit_Clone/SignUp', thisUser: thisUser })
     })
 }
