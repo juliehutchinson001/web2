6eaabd2094599cc915fe7880c1bf64f2cb6e1b3e
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-03 01:00:05 -0800

    fix: change route when rendering form to the page

--- reddit_clone/controllers/subreddit.js
+++ reddit_clone/controllers/subreddit.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.get('/n/:subreddit', function(req, res) {
         const thisUser = req.user;
         Post.find({ subreddit: req.params.subreddit }).then(function(posts) {
-            res.render('index', { posts: posts, pageTitle: req.params.subreddit, thisUser: thisUser })
+            res.render('home', { posts: posts, pageTitle: req.params.subreddit, thisUser: thisUser })
         }).catch(function(err) {
             console.log(err)
         })
