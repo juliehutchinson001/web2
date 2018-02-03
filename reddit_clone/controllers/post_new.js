ac7f35d65b3ed8e4966fc71ea2e8a8df3a1f72c1
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-03 00:57:07 -0800

    feat: get new post

--- reddit_clone/controllers/post_new.js
+++ reddit_clone/controllers/post_new.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
     app.get("/posts/new", function(req, res) {
-        res.render("new_form", { pageTitle: 'New Post' });
+        res.render("new_form", { pageTitle: 'Reddit_Clone/New_Post' });
     })
 }
