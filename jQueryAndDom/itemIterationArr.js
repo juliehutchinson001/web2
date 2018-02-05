8ffa2c6569177e38d317ae8b7a1fe232f4962ae6
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-05 14:24:24 -0800

    init: explanation of array filter

--- jQueryAndDom/itemIterationArr.js
+++ jQueryAndDom/itemIterationArr.js
@@ -3,3 +3,16 @@ let a = [1, 2, 3, 4];
 let b = a.map(function(item, i, arr) {
     return item * item;
 });
+
+let c = a.reduce(function(total, item, i, arr) {
+    return total + item;
+});
+
+let d = [{ price: 1, name: "Apple" }, { price: 2, name: "Orange" }];
+let e = d.reduce(function(tital, item) {
+    return total + item.price;
+}, 0);
+
+let f = a.filter(function() {
+
+})
