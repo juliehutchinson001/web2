7e4fdb94c5c13b4213b89b1a6316f5bc21986ff9
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-13 21:14:04 -0800

    feat: making a mock array of reviews for the project

--- job_documentation_helper/app.js
+++ job_documentation_helper/app.js
@@ -15,3 +15,9 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    { title: "Great Review" },
+    { title: "Next Review" }
+]
