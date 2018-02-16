a83793b12bb72be860654eb55b3cefb1ef368750
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-16 01:42:42 -0800

    fix: remove arrow functionality to base code

--- reddit_clone/controllers/show.js
+++ reddit_clone/controllers/show.js
@@ -1,10 +1,10 @@
 let Post = require("../models/post");
 
 module.exports = function(app) {
-    app.get('/posts/:id', (req, res) => {
+    app.get('/posts/:id', function(req, res) {
         let postId = req.params.id
-        Post.findById(postId).then(post => {
-            res.render('show', { post: post, pageTitle: '<%= post.title%>' })
+        Post.findById(postId).then(function(post) {
+            res.render('show', { post: post, pageTitle: '<%= post.title %>' })
         }).catch(function(err) {
             console.log(err);
         })
