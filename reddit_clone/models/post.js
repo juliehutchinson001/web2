3dd719dbd8555dcb7c2b302db099b4b0dbd72bb8
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-16 01:02:31 -0800

    fix: correction in the href property of comments' schema

--- reddit_clone/models/post.js
+++ reddit_clone/models/post.js
@@ -8,7 +8,7 @@ const PostSchema = new Schema({
     url: { type: String, required: true },
     summary: { type: String, required: true },
     subreddit: { type: String, required: true },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comments' }]
 })
 
 PostSchema.pre('save', function(next) {
