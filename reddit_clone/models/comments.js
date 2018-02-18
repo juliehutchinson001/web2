f33b6481578f28fdb01c94cad0055371ab427716
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-18 10:43:52 -0800

    feat: implement votes and blogposts in comments

--- reddit_clone/models/comments.js
+++ reddit_clone/models/comments.js
@@ -2,10 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    createdAt: { type: Date },
-    updatedAt: { type: Date },
-    content: { type: String, required: true },
-    author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+    title: String,
+    body: String,
+    date: Date
+});
+
+var BlogPost = new Schema({
+    author: ObjectId,
+    title: String,
+    body: String,
+    date: Date,
+    comments: [Comments],
+    meta: {
+        votes: Number,
+        favs: Number
+    }
 });
 
 CommentSchema.pre('save', function(next) {
